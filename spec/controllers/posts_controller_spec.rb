76c396e0a1ceb02fc71db9bd201046338cc1807b
ckrgz001 <ckrgz001@outlook.com>
2017-12-18 16:52:13 -0500

    Checkpoint 18 assignment attempt - problems with create advertisement word rspec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -42,13 +42,11 @@ RSpec.describe PostsController, type: :controller do
     expect(response).to have_http_status(:success)
   end
 
-# #2
   it "renders the #new view" do
     get :new
     expect(response).to render_template :new
   end
 
-# #3
   it "instantiates @post" do
     get :new
     expect(assigns(:post)).not_to be_nil
@@ -56,18 +54,17 @@ RSpec.describe PostsController, type: :controller do
 end
 
 describe "POST create" do
-# #4
   it "increases the number of Post by 1" do
     expect{ post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
   end
 
-# #5
+
   it "assigns the new post to @post" do
     post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
     expect(assigns(:post)).to eq Post.last
   end
 
-# #6
+
   it "redirects to the new post" do
     post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
     expect(response).to redirect_to Post.last
