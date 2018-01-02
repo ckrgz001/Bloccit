3dba92d0e7ea8b3a996bddd76ec81041e09541d6
ckrgz001 <ckrgz001@outlook.com>
2018-01-02 18:47:20 -0500

    removed colon

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
    before_save { self.email = email.downcase if email.present? }
-   before_save : {name = name.split.map{|x| x.capitalize}.join(' ')}
+   before_save {self.name = name.split.map{|x| x.capitalize}.join(' ')}
 
    
      validates :name, length: { minimum: 1, maximum: 100 }, presence: true
